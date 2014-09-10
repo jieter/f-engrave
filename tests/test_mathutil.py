a3dddea12fc4810ec9ad0a771572522c5ec4a844
Jan Pieter Waagmeester <jieter@jieter.nl>
2014-09-10 14:19:15 +0200

    Factor out svg outputting code, add BoundingBox and some tests.

---
+++ tests/test_mathutil.py
@@ -0,0 +1,78 @@
+import unittest
+import csv
+
+from util.mathutil import *
+
+INFINITY = 1e10
+
+
+class BoundingBoxTest(unittest.TestCase):
+
+    def test_initizialize(self):
+        bbox = BoundingBox()
+
+        self.assertEquals(bbox.xmin, INFINITY)
+        self.assertEquals(bbox.xmax, -INFINITY)
+        self.assertEquals(bbox.ymin, INFINITY)
+        self.assertEquals(bbox.ymax, -INFINITY)
+
+        xmin, xmax, ymin, ymax = (0, 12, 123, 400)
+        bbox = BoundingBox(xmin, xmax, ymin, ymax)
+
+        self.assertEquals(bbox.xmin, xmin)
+        self.assertEquals(bbox.xmax, xmax)
+        self.assertEquals(bbox.ymin, ymin)
+        self.assertEquals(bbox.ymax, ymax)
+
+    def test_extend_with_bbox(self):
+        bbox = BoundingBox()
+        bbox.extend(BoundingBox(0, 1.0, 2, 4))
+        bbox.extend(BoundingBox(1, 4, 8, 100))
+
+        self.assertEquals(bbox, BoundingBox(0.0, 4, 2, 100))
+
+    def test_extend_with_2tuple(self):
+        bbox = BoundingBox(0, 1, 2, 4).extend(100, 500)
+
+        self.assertEquals(bbox, BoundingBox(0, 100, 2, 500))
+
+    def test_extend_with_4tuple(self):
+        bbox = BoundingBox(0, 1, 2, 4).extend(0, 1, 3, 8)
+
+        self.assertEquals(bbox, BoundingBox(0, 1, 2, 8))
+
+    def test_extend_with_line(self):
+        bbox = BoundingBox(0, 0, 0, 0)
+
+        bbox.extend(Line((0, 0, 100, 100)))
+
+        self.assertEquals(bbox, BoundingBox(0, 100, 0, 100))
+
+    def test_extend_with_lines(self):
+        bbox = BoundingBox()
+
+        with open('tests/files/lines.csv', 'r') as csvfile:
+            lines = [map(float, line) for line in csv.reader(csvfile, delimiter=',')]
+
+            minx, maxx, miny, maxy = (INFINITY, -INFINITY, INFINITY, -INFINITY)
+            for line in lines:
+                bbox.extend(Line(map(float, line)))
+
+                minx = min(minx, line[0], line[2])
+                maxx = max(maxx, line[0], line[2])
+
+                miny = min(miny, line[1], line[3])
+                maxy = max(maxy, line[1], line[3])
+
+            self.assertEquals(str(bbox), str(BoundingBox(minx, maxx, miny, maxy)))
+
+    def test_pad(self):
+        bbox = BoundingBox(0, 100, 0, 100).pad(10)
+
+        self.assertEquals(bbox, BoundingBox(-10, 110, -10, 110))
+
+    def test_str(self):
+        bbox = BoundingBox(0, 1, 2, 3)
+
+        self.assertEquals(str(bbox), 'BoundingBox([0.0, 1.0, 2.0, 3.0])')
+
---
+++ writers/svg.py
@@ -0,0 +1,71 @@
+from util.mathutil import BoundingBox, Line
+
+def svg(app):
+    if app.cut_type.get() == "v-carve":
+        thickness = 0.001
+    else:
+        thickness = float(app.STHICK.get())
+
+    dpi = 100
+
+    bbox = BoundingBox()
+    for line in app.coords:
+        bbox.extend(Line(line[0:4]))
+
+    # XOrigin, YOrigin = app.get_origin()
+    # Radius_plot=  float(app.RADIUS_PLOT)
+    # if Radius_plot != 0:
+    #     maxx = max(maxx, XOrigin + Radius_plot - app.Xzero)
+    #     minx = min(minx, XOrigin - Radius_plot - app.Xzero)
+    #     miny = min(miny, YOrigin - Radius_plot - app.Yzero)
+    #     maxy = max(maxy, YOrigin + Radius_plot - app.Yzero)
+
+    print bbox
+    bbox.pad(thickness / 2)
+    print bbox
+    width_in  = bbox.xmax - bbox.xmin
+    height_in = bbox.ymax - bbox.ymin
+
+    width  = width_in * dpi
+    height = height_in * dpi
+
+    print bbox, thickness, width, height, width_in, height_in
+
+    svgcode = []
+    svgcode.append('''
+<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="%(width_in)f%(units)s" height="%(height_in)f%(units)s" \
+viewBox="0 0 %(width)f %(height)f" xmlns="http://www.w3.org/2000/svg" version="1.1">
+    <title>F-engrave Output</title>
+    <desc>SVG File Created By F-Engrave</desc>
+    ''' % {
+            'width_in': width_in,
+            'height_in': height_in,
+            'units': app.units.get(),
+            'width': width,
+            'height': height
+        })
+
+    # # Make Circle
+    # if Radius_plot != 0 and app.cut_type.get() == "engrave":
+    #     svgcode.append('    <circle cx="%f" cy="%f" r="%f" fill="none" stroke="blue" stroke-width="%f" />' % (
+    #                 ( XOrigin - app.Xzero - minx) * dpi,
+    #                 (-YOrigin + app.Yzero + maxy) * dpi,
+    #                   Radius_plot               ) * dpi,
+    #                   thickness                   * dpi)
+    # # End Circle
+
+    for line in app.coords:
+        XY = line
+        svgcode.append('  <path d="M %f %f L %f %f" fill="none" stroke="blue" stroke-width="%f" stroke-linecap="round" stroke-linejoin="round" />' % (
+                ( XY[0] - bbox.xmin) * dpi,
+                (-XY[1] + bbox.ymax) * dpi,
+                ( XY[2] - bbox.xmin) * dpi,
+                (-XY[3] + bbox.ymax) * dpi,
+                thickness            * dpi))
+
+    svgcode.append('</svg>')
+
+    return svgcode
+
